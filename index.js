5cd2a2f3ca024c71c458211ccb0d4d0a0077dd61
verozou <zw15855241852@163.com>
2024-02-23 14:15:11 +0000

    update yaml and server file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express'
-const app = express()
-app.use(express.static("./dist"))
-const port = process.env.PORT || 8080;
+import { resolve, dirname } from 'path'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname )
+const app = express();
+app.use(express.static(resolve(__dirname + '/dist')));
+app.get('*', (req, res) => {
+    const html = readFileSync(__dirname + '/dist/index.html', 'utf-8')
+    res.send(html)
+})
+const port = 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
-})
+});
